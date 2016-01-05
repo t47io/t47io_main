9957ffc1ba4fbf605fdcf42fa13697a0ab71cf4a
t47io <t47@alumni.stanford.edu>
2016-01-05 13:40:13 -0500

    add chmod

---
+++ util_chmod.sh
@@ -0,0 +1,16 @@
+sudo chown -R admin:www-data *
+sudo chmod 640 *.html
+
+sudo chmod 640 $(find css -type f)
+sudo chmod 750 $(find css -type d)
+sudo chmod 640 $(find fonts -type f)
+sudo chmod 750 $(find fonts -type d)
+sudo chmod 640 $(find img -type f)
+sudo chmod 750 $(find img -type d)
+sudo chmod 640 $(find js -type f)
+sudo chmod 750 $(find js -type d)
+sudo chmod 640 $(find pdf -type f)
+sudo chmod 750 $(find pdf -type d)
+
+sudo chown -R admin:admin *.sh
+sudo chmod -R 700 *.sh
