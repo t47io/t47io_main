5953e4c130c42a8da82ce1bfd48ba48acb915b20
t47io <t47@alumni.stanford.edu>
2016-01-25 12:46:21 -0800

    init nodejs

--- util_chmod.sh
+++ util_chmod.sh
@@ -1,18 +1,11 @@
 sudo chown -R admin:www-data *
-sudo chmod 640 *.html *.txt *.md
+sudo chmod 640 *.js *.txt *.md
 
-sudo chmod 640 $(find css -type f)
-sudo chmod 750 $(find css -type d)
-sudo chmod 640 $(find fonts -type f)
-sudo chmod 750 $(find fonts -type d)
-sudo chmod 640 $(find html -type f)
-sudo chmod 750 $(find html -type d)
-sudo chmod 640 $(find img -type f)
-sudo chmod 750 $(find img -type d)
-sudo chmod 640 $(find js -type f)
-sudo chmod 750 $(find js -type d)
-sudo chmod 640 $(find pdf -type f)
-sudo chmod 750 $(find pdf -type d)
+sudo chmod 640 $(find public -type f)
+sudo chmod 750 $(find public -type d)
+
+sudo chmod 640 $(find config -type f)
+sudo chmod 750 $(find config -type d)
 
 sudo chown -R admin:admin *.sh
 sudo chmod -R 700 *.sh
