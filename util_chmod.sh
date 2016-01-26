f7ff0ef7a8953ecc6a52e52f03650655d2b20f93
T47 <t47@stanford.edu>
2016-01-25 23:28:15 -0800

    add temp project links

--- util_chmod.sh
+++ util_chmod.sh
@@ -1,9 +1,11 @@
 sudo chown -R admin:www-data *
+sudo chown -R admin:admin node_modules
 sudo chmod 640 *.js* *.md
 
 sudo chmod 640 $(find public -type f)
 sudo chmod 750 $(find public -type d)
-
+sudo chmod 640 $(find views -type f)
+sudo chmod 750 $(find views -type d)
 sudo chmod 640 $(find config -type f)
 sudo chmod 750 $(find config -type d)
 
