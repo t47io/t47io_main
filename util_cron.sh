b7d01f1df6d72ef70edae1258b61bae6733e48ae
t47io <t47@alumni.stanford.edu>
2016-03-09 18:52:54 -0800

    simplify cron chmod

--- util_cron.sh
+++ util_cron.sh
@@ -3,7 +3,4 @@ cd /home/admin/t47io/main
 python stats_git.py
 python stats_pub.py
 
-chown -R admin:admin data
-chown admin:www-data config/dat.json
-chmod 640 data/*.json
-chmod 640 config/dat.json
+./util_chmod.sh
