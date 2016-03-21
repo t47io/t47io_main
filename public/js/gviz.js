037d27fd3a1fe1166209b1d235e8ba78992b28d8
t47io <t47@alumni.stanford.edu>
2016-03-20 23:56:18 -0700

    use CSS place_holder anim

--- public/js/gviz.js
+++ public/js/gviz.js
@@ -4,7 +4,7 @@ var gviz_handles = [];
 
 function readyHandler() {
     $(".place_holder").each(function() {
-        if ($(this).html().length > 0) { $(this).removeClass("place_holder"); }
+        if ($(this).find('.center-block').length === 0) { $(this).removeClass("place_holder"); }
     });
 }
 
@@ -39,6 +39,7 @@ function drawGIT(data) {
                 'title': 'Count (#)',
                 'titleTextStyle': {'bold': true},
                 // 'scaleType': 'log'
+                'format': '#'
             },
             'hAxis': {
                 'gridlines': {'count': -1},
@@ -68,6 +69,7 @@ function drawGIT(data) {
                 'title': 'Count (#)',
                 'titleTextStyle': {'bold': true},
                 'scaleType': 'mirrorLog',
+                'format': 'scientific',
                 'gridlines': {'count': 5}
             },
             'hAxis': {
@@ -91,7 +93,7 @@ function drawChart() {
     var html = "", i = 0, name = "";
     for (i = 0; i < repo.length; i++) {
         name = repo[i];
-        html += '<div class="row"><div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"><p id="git-name-' + name + '"></p><p id="git-url-' + name + '"></p><p id="git-label-' + name + '"></p><br/><table class="table table-hover"><thead><tr class="active"><th class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><i class="fa fa-fw fa-user"></i>&nbsp;&nbsp;Account</th><th class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><i class="fa fa-fw fa-chevron-circle-up"></i>&nbsp;&nbsp;Commits</th><th class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><i class="fa fa-fw fa-plus-circle"></i>&nbsp;&nbsp;Additions</th><th class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><i class="fa fa-fw fa-minus-circle"></i>&nbsp;&nbsp;Deletions</th></tr></thead><tbody id="git-tb-' + name + '"></tbody></table></div><div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"><div id="plot_c_' + name + '" class="thumbnail git place_holder" style="height: 175px;"></div><div id="plot_ad_' + name + '" class="thumbnail git place_holder" style="height: 175px;"></div></div></div>';
+        html += '<div class="row"><div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"><p id="git-name-' + name + '"></p><p id="git-url-' + name + '"></p><p id="git-label-' + name + '"></p><br/><table class="table table-hover"><thead><tr class="active"><th class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><i class="fa fa-fw fa-user"></i>&nbsp;&nbsp;Account</th><th class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><i class="fa fa-fw fa-chevron-circle-up"></i>&nbsp;&nbsp;Commits</th><th class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><i class="fa fa-fw fa-plus-circle"></i>&nbsp;&nbsp;Additions</th><th class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><i class="fa fa-fw fa-minus-circle"></i>&nbsp;&nbsp;Deletions</th></tr></thead><tbody id="git-tb-' + name + '"></tbody></table></div><div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"><div id="plot_c_' + name + '" class="thumbnail git place_holder"><div class="center-block"><div class="cube"> <div class="ani1"> <div class="front"><i></i><i></i><i></i></div><div class="left"><i></i><i></i><i></i></div></div> <div class="ani2"> <div class="front"><i></i><i></i><i></i></div> <div class="bottom"><i></i><i></i><i></i></div></div><div class="ani3"><div class="front"><i></i><i></i><i></i></div><div class="right"><i></i><i></i><i></i></div></div><div class="ani4"><div class="front"><i></i><i></i><i></i></div><div class="top"><i></i><i></i><i></i></div></div><div class="shadow"></div></div></div></div><div id="plot_ad_' + name + '" class="thumbnail git place_holder"><div class="center-block"><div class="cube"> <div class="ani1"> <div class="front"><i></i><i></i><i></i></div><div class="left"><i></i><i></i><i></i></div></div> <div class="ani2"> <div class="front"><i></i><i></i><i></i></div> <div class="bottom"><i></i><i></i><i></i></div></div><div class="ani3"><div class="front"><i></i><i></i><i></i></div><div class="right"><i></i><i></i><i></i></div></div><div class="ani4"><div class="front"><i></i><i></i><i></i></div><div class="top"><i></i><i></i><i></i></div></div><div class="shadow"></div></div></div></div></div></div>';
         if (i < repo.length - 1) { html += '<hr/>'; }
     }
     $("#git_body").html(html).removeClass("place_holder");
@@ -104,5 +106,9 @@ function drawChart() {
             success: drawGIT
         });
     }
+
 }
+
+
+
 
