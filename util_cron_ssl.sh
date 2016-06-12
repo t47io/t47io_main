93935ac5ed8eb925d12193ab850f42ba8b0ebb98
t47io <t47@alumni.stanford.edu>
2016-06-12 00:56:47 +0000

    colorize ssl script

--- util_cron_ssl.sh
+++ util_cron_ssl.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 service nginx stop
 
-/home/admin/letsencrypt/letsencrypt-auto renew > /home/admin/log_ssl.log 2>&1
+echo >> /home/admin/log_ssl.log
+echo "$(tput setab 2)$(date)$(tput sgr 0)" >> /home/admin/log_ssl.log
+/home/admin/letsencrypt/letsencrypt-auto renew >> /home/admin/log_ssl.log 2>&1
 LE_STATUS=$?
+echo >> /home/admin/log_ssl.log
+
+service nginx start
 
 if [ "$LE_STATUS" != 0 ]; then
-    echo Automated renewal failed:
+    echo "$(tput setab 1)Automated renewal failed:$(tput sgr 0)"
     cat /home/admin/log_ssl.log
     exit 1
 fi
-
-service nginx start
