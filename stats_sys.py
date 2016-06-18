ffbee68d601d2de11b38692e979c3e98b1bebedf
t47io <t47@alumni.stanford.edu>
2016-06-18 15:04:03 -0700

    fix ver dict key/val

--- stats_sys.py
+++ stats_sys.py
@@ -10,7 +10,7 @@ d = time.strftime('%Y%m%d')  # datetime.datetime.now().strftime('%Y%m%d')
 t0 = time.time()
 MEDIA_ROOT = os.getcwd()
 ver = {}
-print("Checking system versions...")
+print "Checking system versions..."
 
 # line 1
 ver['ubuntu'] = subprocess.Popen("lsb_release -a | head -3 | tail -1 | sed 's/.*Ubuntu //g' | sed 's/ .*//g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
@@ -20,7 +20,7 @@ ver['screen'] = subprocess.Popen("screen --version | sed 's/.*version//g' | sed 
 ver['bash'] = subprocess.Popen("bash --version | head -1 | sed 's/.*version//g' | sed 's/-release.*//g' | sed 's/[ ()]//g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 open(os.path.join(MEDIA_ROOT, 'data/temp.txt'), 'w').write(subprocess.Popen('ssh -V', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip())
 ver['ssh'] = subprocess.Popen("sed 's/^OpenSSH\_//g' %s | sed 's/U.*//' | sed 's/,.*//g' | sed 's/[a-z]/./g'" % os.path.join(MEDIA_ROOT, 'data/temp.txt'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print("\033[92mSUCCESS\033[0m: line \033[94m1\033[0m"),
+print "\033[92mSUCCESS\033[0m: line \033[94m1\033[0m",
 
 # line 2
 ver['curl'] = subprocess.Popen("curl --version | head -1 | sed 's/.*curl //g' | sed 's/ (.*//g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
@@ -29,7 +29,7 @@ ver['tar'] = subprocess.Popen("tar --version | head -1 | sed 's/.*)//g' | sed 's
 ver['pandoc'] = subprocess.Popen("pandoc --version | head -1 | sed 's/.*pandoc //g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 ver['imagemagick'] = subprocess.Popen("mogrify -version | head -1 | sed 's/\-.*//g' | sed 's/.*ImageMagick //g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 ver['nano'] = subprocess.Popen("nano --version | head -1 | sed 's/.*version //g' | sed 's/(.*//g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m2\033[0m"),
+print ", \033[94m2\033[0m",
 
 # line 3
 ver['python'] = '%s.%s.%s' % (sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
@@ -40,7 +40,7 @@ ver['node'] = subprocess.Popen("node --version | sed 's/.*v//g'", shell=True, st
 ver['sass'] = subprocess.Popen("sass --version | sed 's/.*Sass //g' | sed 's/ (.*//g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 ver['gcc'] = subprocess.Popen("gcc --version | head -1 | sed 's/.*) //g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 ver['make'] = subprocess.Popen("make --version | head -1 | sed 's/.*Make//g' | sed 's/ //g' | head -1", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip().split('\n')[0]
-print(", \033[94m3\033[0m"),
+print ", \033[94m3\033[0m",
 
 # line 4
 ver['django'] = subprocess.Popen('python -c "import django; print django.__version__"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
@@ -52,7 +52,7 @@ ver['django_suit'] = subprocess.Popen('python -c "import suit; print suit.VERSIO
 ver['django_adminplus'] = subprocess.Popen('python -c "import adminplus; print adminplus.__version__"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 open(os.path.join(MEDIA_ROOT, 'data/temp.txt'), 'w').write(subprocess.Popen('pip show django-filemanager', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip())
 ver['django_filemanager'] = subprocess.Popen("head -4 %s | tail -1 | sed 's/.*: //g'" % os.path.join(MEDIA_ROOT, 'data/temp.txt'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m4\033[0m"),
+print ", \033[94m4\033[0m",
 
 # line 5
 ver['tkinter'] = subprocess.Popen('python -c "import Tkinter; print Tkinter.Tcl().eval(\'info patchlevel\')"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
@@ -63,7 +63,7 @@ ver['slacker'] = subprocess.Popen("head -4 %s | tail -1 | sed 's/.*: //g'" % os.
 ver['dropbox'] = subprocess.Popen('python -c "import dropbox; print dropbox.__version__"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 open(os.path.join(MEDIA_ROOT, 'data/temp.txt'), 'w').write(subprocess.Popen('pip show gviz-api.py', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip())
 ver['gviz'] = subprocess.Popen("head -4 %s | tail -1 | sed 's/.*: //g'" % os.path.join(MEDIA_ROOT, 'data/temp.txt'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m5\033[0m"),
+print ", \033[94m5\033[0m",
 
 # line 6
 ver['requests'] = subprocess.Popen('python -c "import requests; print requests.__version__"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
@@ -72,42 +72,42 @@ open(os.path.join(MEDIA_ROOT, 'data/temp.txt'), 'w').write(subprocess.Popen('pip
 ver['pygithub'] = subprocess.Popen("head -4 %s | tail -1 | sed 's/.*: //g'" % os.path.join(MEDIA_ROOT, 'data/temp.txt'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 ver['beautifulsoup'] = subprocess.Popen('python -c "import bs4; print bs4.__version__"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 ver['html5lib'] = subprocess.Popen('python -c "import html5lib; print html5lib.__version__[0:5]"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m6\033[0m"),
+print ", \033[94m6\033[0m",
 
 # line 7
 open(os.path.join(MEDIA_ROOT, 'data/temp.txt'), 'w').write(subprocess.Popen('npm list', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip())
 for key in ['express', 'helmet', 'sanitizer', 'nunjucks', 'glob', 'striptags']:
     ver[key] = subprocess.Popen("cat %s | grep %s | head -1 | sed 's/.*@//g' | sed 's/ .*//g'" % (os.path.join(MEDIA_ROOT, 'data/temp.txt'), key), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m7\033[0m"),
+print ", \033[94m7\033[0m",
 
 # line 8
 for key in ['jshint', 'jshint-stylish', 'body-parser', 'nodemailer', 'supervisor']:
     ver[key] = subprocess.Popen("cat %s | grep %s | head -1 | sed 's/.*@//g' | sed 's/ .*//g'" % (os.path.join(MEDIA_ROOT, 'data/temp.txt'), key), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m8\033[0m"),
+print ", \033[94m8\033[0m",
 
 # line 9
 ver['grunt'] = subprocess.Popen("grunt --version | head -2 | head -1 | sed 's/.*v//g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 for key in ['clean', 'concat', 'copy', 'sass']:
     ver['grunt_contrib_%s' % key] = subprocess.Popen("cat %s | grep grunt-contrib-%s | head -1 | sed 's/.*@//g' | sed 's/ .*//g'" % (os.path.join(MEDIA_ROOT, 'data/temp.txt'), key), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip().split('\n')[0]
 ver['node-sass'] = subprocess.Popen("cat %s | grep node-sass-middleware | head -1 | sed 's/.*@//g' | sed 's/ .*//g'" % os.path.join(MEDIA_ROOT, 'data/temp.txt'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip().split('\n')[0]
-print(", \033[94m9\033[0m"),
+print ", \033[94m9\033[0m",
 
 # line 10
 for key in ['jshint', 'cssmin', 'htmlmin', 'imagemin', 'uglify', 'watch']:
     ver['grunt_contrib_%s' % key] = subprocess.Popen("cat %s | grep grunt-contrib-%s | head -1 | sed 's/.*@//g' | sed 's/ .*//g'" % (os.path.join(MEDIA_ROOT, 'data/temp.txt'), key), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip().split('\n')[0]
-print(", \033[94m10\033[0m"),
+print ", \033[94m10\033[0m",
 
 # line 11
 ver['bower'] = subprocess.Popen("bower --version", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 open(os.path.join(MEDIA_ROOT, 'data/temp.txt'), 'w').write(subprocess.Popen('bower list', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip())
 for key in ['bootstrap', 'jquery', 'font-awesome', 'isotope', 'gsap']:
     ver[key] = subprocess.Popen("cat %s | grep %s | head -1 | sed 's/.*%s//g' | sed 's/ .*//g' | sed 's/\#//g'" % (os.path.join(MEDIA_ROOT, 'data/temp.txt'), key, key), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m11\033[0m"),
+print ", \033[94m11\033[0m",
 
 # line 12
 for key in ['scrollmagic', 'moment', 'd3', 'fullcalendar', 'headjs']:
     ver[key] = subprocess.Popen("cat %s | grep %s | head -1 | sed 's/.*%s//g' | sed 's/ .*//g' | sed 's/\#//g'" % (os.path.join(MEDIA_ROOT, 'data/temp.txt'), key, key), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m12\033[0m"),
+print ", \033[94m12\033[0m",
 
 # line 13
 open(os.path.join(MEDIA_ROOT, 'data/temp.txt'), 'w').write(subprocess.Popen('nginx -v', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip())
@@ -118,7 +118,7 @@ ver['openssl'] = subprocess.Popen("openssl version | sed 's/.*OpenSSL //g' | sed
 open(os.path.join(MEDIA_ROOT, 'data/temp.txt'), 'w').write(subprocess.Popen('~/letsencrypt/letsencrypt-auto --version', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip())
 ver['letsencrypt'] = subprocess.Popen("tail -1 %s | sed 's/.* //g'" % os.path.join(MEDIA_ROOT, 'data/temp.txt'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 ver['mysql'] = subprocess.Popen("mysql --version | sed 's/,.*//g' | sed 's/.*Distrib //g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m13\033[0m"),
+print ", \033[94m13\033[0m",
 
 # line 14
 ver['git'] = subprocess.Popen("git --version | sed 's/.*version //g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
@@ -127,16 +127,16 @@ ver['npm'] = subprocess.Popen('npm --version', shell=True, stdout=subprocess.PIP
 ver['gem'] = subprocess.Popen('gem --version', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 ver['htop'] = subprocess.Popen("htop --version | head -1 | sed 's/.*htop //g' | sed 's/ \-.*//g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
 ver['yuicompressor'] = subprocess.Popen("java -jar %s/../yuicompressor.jar -V" % MEDIA_ROOT, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
-print(", \033[94m14\033[0m.")
+print ", \033[94m14\033[0m."
 
 
 subprocess.Popen('rm %s' % os.path.join(MEDIA_ROOT, 'data/temp.txt'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-for key, value in ver.iteritems():
-    value = re.findall(r'[\d\.]+', value)[-1]
+for key in ver:
+    ver[key] = re.findall(r'[\d\.]+', ver[key])[-1]
 print simplejson.dumps(ver, indent=' ' * 4, sort_keys=True)
 
 
-print("\033[92mSUCCESS\033[0m: \033[94mVersions\033[0m recorded in cache/stat_sys.json and cache/stat_ver.json.")
-print("All done successfully!")
-print("Time elapsed: %.1f s." % (time.time() - t0))
+print "\033[92mSUCCESS\033[0m: \033[94mVersions\033[0m recorded in cache/stat_sys.json and cache/stat_ver.json."
+print "All done successfully!"
+print "Time elapsed: %.1f s." % (time.time() - t0)
 
