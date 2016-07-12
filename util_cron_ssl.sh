e0af3a34570345900c2980c43ced4af654fb2443
t47io <t47@alumni.stanford.edu>
2016-07-11 18:06:11 -0700

    automate ssl renewal

--- util_cron_ssl.sh
+++ util_cron_ssl.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-service nginx stop
+if [ "$(cat /home/admin/t47io/main/data/sys_ssl.txt)" == 1 ]; then
+    service nginx stop
 
-echo >> /home/admin/log_ssl.log
-echo "$(tput setab 2)$(date)$(tput sgr 0)" >> /home/admin/log_ssl.log
-/home/admin/letsencrypt/letsencrypt-auto renew >> /home/admin/log_ssl.log 2>&1
-LE_STATUS=$?
-echo >> /home/admin/log_ssl.log
+    echo >> /home/admin/log_ssl.log
+    echo "$(tput setab 2)$(date)$(tput sgr 0)" >> /home/admin/log_ssl.log
+    /home/admin/letsencrypt/letsencrypt-auto renew >> /home/admin/log_ssl.log 2>&1
+    LE_STATUS=$?
+    echo >> /home/admin/log_ssl.log
 
-service nginx start
+    service nginx start
 
-if [ "$LE_STATUS" != 0 ]; then
-    echo "$(tput setab 1)Automated renewal failed:$(tput sgr 0)"
-    cat /home/admin/log_ssl.log
-    exit 1
+    if [ "$LE_STATUS" != 0 ]; then
+        echo "$(tput setab 1)Automated renewal failed:$(tput sgr 0)"
+        cat /home/admin/log_ssl.log
+        exit 1
+    fi
 fi
