0b2205115f0920cc054b18aea206de30855b856f
t47io <t47@alumni.stanford.edu>
2016-08-18 07:12:26 +0000

    chmod node/bower

--- util_chmod.sh
+++ util_chmod.sh
@@ -1,7 +1,6 @@
 sudo chown -R admin:www-data *
 sudo chown -R admin:admin data node_modules bower_components
 sudo chown admin:admin *.py *.js *.json *.md *.txt
-sudo chmod 600 *.js* *.md *.py
 
 find src -type f | sudo xargs chmod 640
 find src -type d | sudo xargs chmod 750
@@ -12,7 +11,14 @@ find config -type d | sudo xargs chmod 750
 find data -type f | sudo xargs chmod 640
 find data -type d | sudo xargs chmod 750
 
-sudo chown -R admin:admin *.sh .git
-sudo chmod -R 700 *.sh
+sudo chmod 600 *.js* *.md *.py
+sudo chown admin:admin *.sh .git
+sudo chmod 700 *.sh
 find .git -type f | sudo xargs chmod 640
 find .git -type d | sudo xargs chmod 750
+
+find bower_components -type f | sudo xargs chmod 640
+find bower_components -type d | sudo xargs chmod 750
+find node_modules -type f | sudo xargs chmod 640
+find node_modules -type d | sudo xargs chmod 750
+
