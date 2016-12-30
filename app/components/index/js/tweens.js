8b297c731414f2b5a76b0aa1795fd5299b20d8c0
t47io <t47@alumni.stanford.edu>
2016-12-30 18:34:44 -0500

    finish home animation; move factory js

--- app/components/index/js/tweens.js
+++ app/components/index/js/tweens.js
@@ -5,7 +5,7 @@ const func = {
     const increment = (valTo - valFrom) / loops;
     const interval = setInterval(() => {
       valNow += increment;
-      loopNow += 1
+      loopNow += 1;
       setState({value: valNow, status: "counting"});
 
       if (loopNow == loops) {
@@ -14,11 +14,31 @@ const func = {
       }
     }, 100);
   },
-  onUp: () => {
-    console.log('up')
+  typeWrite: (valTo, state, setState) => {
+    return new Promise((resolve, reject) => {
+      let valNow = "", loopNow = 0;
+      const loops = valTo.length;
+
+      const interval = setInterval(() => {
+        valNow = valTo.slice(0, loopNow);
+        valNow += '';
+        setState({...state, title: valNow.replace(/\!/g, '').replace(/\@/g, '<br/>')});
+        loopNow += 1;
+
+        if (loopNow == loops + 1) {
+          clearInterval(interval);
+          resolve(1);
+        }
+      }, 100);
+    });
   },
-  onDown: () => {
-    console.log('down')
+  delay: (time, callback) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (typeof callback === 'function') { callback(); }
+        resolve();
+      }, time);
+    });
   }
 };
 
@@ -31,8 +51,39 @@ const header = {
 };
 
 
-const home = {};
+const home = {
+  name: {
+    start: {transform: 'scale(5)', ...fadeStart},
+    end: {transform: 'scale(1)', ...fadeEnd}
+  },
+  title: (state, title, setState) => {
+    if (!state.isPlayed) {
+      setState({...state, isPlayed: true});
 
+      func.delay(1600)
+      .then(() => func.typeWrite(title, state, setState))
+      .then(() => {
+        func.delay(600, () => setState({...state, isBlink: false}));
+        return func.delay(1000);
+      })
+      .then(() => {
+        setState({...state, isShade: false});
+      });
+    }
+  }
+};
+//       colorArrow: KUTE.fromTo(".HOME__scroll_down i",
+//         {color: '#fff'},
+//         {color: '#9fc906'},
+//         {repeat: Infinity, duration: 5000}),
+//       fadeTitle: KUTE.fromTo(".HOME__content",
+//         fadeEnd,
+//         fadeHalf,
+//         {...reversable, ...playOneSec}),
+//       fadeScroll: KUTE.fromTo(".HOME__scroll_down",
+//         fadeHalf,
+//         fadeEnd,
+//         {...reversable, ...playOneSec})
 const about = {
   header,
   icon: (offset) => ({
@@ -47,11 +98,11 @@ const affiliation = {header};
 const portfolio = {
   header,
   menu: (offset) => ({
-    topBottom: {transform: 'translateY(100%)', ...fadeStart},
+    centerBottom: {transform: 'translateY(100%)', ...fadeStart},
     [`bottomBottom+${offset}`]: {transform: 'translateY(0%)', ...fadeEnd}
   }),
   thumbnail: {
-    ease: 'bouncePast',
+    ease: 'swingFromTo',
     topBottom: {transform: 'translateY(100%) scale(2)'},
     bottomBottom: {transform: 'translateY(0%) scale(1)'}
   }
@@ -74,130 +125,44 @@ const skills = {
 const stats = {
   header,
   counter: (offset) => ({
-    topBottom: {transform: 'rotateY(360deg) scale(0)'},
+    centerBottom: {transform: 'rotateY(360deg) scale(0)'},
     [`bottomBottom+${offset}`]: {transform: 'rotateY(0deg) scale(1)'}
   }),
   git: {
     ease: 'easeInBack',
-    topBottom: {transform: 'rotateY(180deg)', ...fadeQuarter},
-    bottomBottom: {transform: 'rotateY(0deg)', ...fadeEnd}
+    centerBottom: {transform: 'rotateY(180deg)', ...fadeQuarter},
+    'bottomPct+75': {transform: 'rotateY(0deg)', ...fadeEnd}
   }
 };
 
-const pubs = {};
+const pubs = {
+  header,
+  year: {
+    bottomBottom: {transform: 'rotateX(180deg)', ...fadeQuarter},
+    'bottomPct+75': {transform: 'rotateX(0deg)', ...fadeEnd}
+  },
+  entry: {
+    topBottom: {transform: 'rotateX(180deg)', ...fadeQuarter},
+    'topPct+75': {transform: 'rotateX(0deg)', ...fadeEnd}
+  }
+};
 
-const contact = {};
+const contact = {
+  header,
+  icon: {
+    ease: 'easeInOutBack',
+    centerBottom: {transform: 'rotateX(360deg) rotate(-45deg)', ...fadeQuarter},
+    'bottomPct+75': {transform: 'rotateX(0deg) rotate(45deg)', ...fadeEnd}
+  },
+  listLeft: {
+    topBottom: {transform: 'translateX(-100%) translateY(100%)', ...fadeQuarter},
+    'topPct+75': {transform: 'translateX(0%) translateY(0%)', ...fadeEnd}
+  },
+  formRight: {
+    topBottom: {transform: 'translateX(100%) translateY(100%)', ...fadeQuarter},
+    'topPct+75': {transform: 'translateX(0%) translateY(0%)', ...fadeEnd}
+  }
+};
 
 
 export {home, about, affiliation, portfolio, skills, stats, pubs, contact, func};
-
-
-
-
-// const headerTween = (target) => (
-//   KUTE.fromTo(target,
-//     {translateY: '-100%', ...fadeStart},
-//     {translateY: 0, ...fadeEnd},
-//     {...reversable, ...playOneSec})
-// );
-
-// const blinkTween = (selector) => (
-//     KUTE.fromTo(selector,
-//       fadeEnd, fadeStart,
-//       {duration: 200, repeat: 2,
-//         complete: () => { $(selector).remove(); }
-//       })
-//     .start()
-// );
-
-// const tweens = {
-//     home: {
-//       name: KUTE.fromTo("#img_name", 
-//         {scale: $(window).width() / 500, ...fadeStart}, 
-//         {scale: 1, ...fadeEnd},
-//         {delay: 1000, ...playOneSec}),
-//       typeWrite1: KUTE.to("#subtitle_1",
-//         {text: 'Full-Stack Web Developer'},
-//         {delay: 2250, duration: 3000,
-//           complete: () => { blinkTween(".HOME__cursor"); }}
-//         ),
-//       typeWrite2: KUTE.to("#subtitle_2",
-//         {text: 'RNA Biochemist & Automator'},
-//         {delay: 5850, duration: 3200,
-//           complete: () => { blinkTween(".HOME__cursor"); }}
-//         ),
-//       brighten: KUTE.to(".HOME__shade",
-//         {backgroundColor: 'rgba(0, 7, 11, 0.25)'},
-//         {delay: 9000, ...playOneSec}),
-//       colorArrow: KUTE.fromTo(".HOME__scroll_down i",
-//         {color: '#fff'},
-//         {color: '#9fc906'},
-//         {repeat: Infinity, duration: 5000}),
-//       fadeTitle: KUTE.fromTo(".HOME__content",
-//         fadeEnd,
-//         fadeHalf,
-//         {...reversable, ...playOneSec}),
-//       fadeScroll: KUTE.fromTo(".HOME__scroll_down",
-//         fadeHalf,
-//         fadeEnd,
-//         {...reversable, ...playOneSec})
-//     },
-//     stats: {
-//       countUp: {
-//         project: KUTE.fromTo("#STATS__counter_1",
-//           {number: 0},
-//           {number: $("#STATS__counter_1").text()},
-//           {delay: 700, duration: 1500}),
-//         code: KUTE.fromTo("#STATS__counter_2",
-//           {number: 0},
-//           {number: $("#STATS__counter_2").text()},
-//           {delay: 900, duration: 2500}),
-//         publication: KUTE.fromTo("#STATS__counter_3",
-//           {number: 0},
-//           {number: $("#STATS__counter_3").text()},
-//           {delay: 1100, duration: 1500}),
-//         scholarship: KUTE.fromTo("#STATS__counter_4",
-//           {number: 0},
-//           {number: $("#STATS__counter_4").text()},
-//           {delay: 1300, duration: 1500})
-//       },
-//     },
-//     pubs: {
-//       header: headerTween(".PUBS__header"),
-//       flipEntry: KUTE.allFromTo(".PUBS__entry",
-//         {rotateX: 180, ...fadeQuarter},
-//         {rotateX: 0, ...fadeEnd},
-//         {delay: 1000, offset: 200, ...playOneSec}),
-//       flipYear: KUTE.allFromTo(".PUBS__year",
-//         {rotateX: 180, ...fadeQuarter},
-//         {rotateX: 0, ...fadeEnd},
-//         {delay: 500, offset: 200, ...playOneSec})
-//     },
-//     contact: {
-//       header: headerTween(".CONTACT__header"),
-//       flipIcon: KUTE.allFromTo(".CONTACT__icon",
-//         {rotateX: 0, ...fadeQuarter},
-//         {rotateX: 360, ...fadeEnd},
-//         {delay: 200, offset: 200, easing: 'easingElasticOut', ...playOneSec}),
-//       rotateIcon: KUTE.allFromTo(".CONTACT__icon",
-//         {rotate: 0, ...fadeQuarter},
-//         {rotate: 45, ...fadeEnd},
-//         {delay: 1200, offset: 200, easing: 'easingElasticOut', duration: 200}),
-//       showListLeft: KUTE.allFromTo(".CONTACT__address li",
-//         {translateX: '-100%', translateY: '100%', ...fadeQuarter},
-//         {translateX: 0, translateY: 0, ...fadeEnd},
-//         {delay: 500, offset: 100, ...playOneSec}),
-//       showFormRight: KUTE.allFromTo(".CONTACT__form .form-group",
-//         {translateX: '100%', translateY: '100%', ...fadeQuarter},
-//         {translateX: 0, translateY: 0, ...fadeEnd},
-//         {delay: 500, offset: 100, ...playOneSec})
-//     },
-//     footer: KUTE.fromTo(".FOOTER__header",
-//       {translateY: '-100%', ...fadeStart},
-//       {translateY: 0, ...fadeEnd},
-//       playOneSec)
-
-// };
-
-
-// export default tweens;
--- app/components/index/jsx/home.jsx
+++ app/components/index/jsx/home.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
 
+import {home as tween, func} from '../js/tweens.js';
 
-const HomeSection = () => (
-  <section id="HOME__section" className="parallax" style="background-image:url('/img/t47/t47_avatar.jpg')">
-    <div className="cover HOME__shade"></div>
-    <div className="container">
-      <div className="HOME__content text-white">
-        <img src='/img/t47/t47_name_2_line.gif' alt="Siqi Tian" width="480" id="img_name" style="opacity:0;" />
-        <p className="text-white" id="subtitle_0" style="margin-top: -10px;">&nbsp;</p>
-        <p className="text-white" id="subtitle_1" style="height: 25px;"></p>
-        <p className="text-white" id="subtitle_2" style="height: 25px;"></p>
-      </div>
-      <div className="HOME__scroll_down">
-        <i className="fa fa-3x fa-fw fa-arrow-circle-down"></i>
-      </div>
 
-    </div>
-  </section>
-);
+class HomeSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPlayed: false,
+      isBlink: true,
+      isShade: true,
+      isName: false,
+      title: "",
+    };
+  }
+
+  componentDidMount() {
+    func.delay(800)
+    .then(() => this.setState({...(this.state), isName: true}))
+    .then(() => tween.title(this.state, this.props.title, this.setState.bind(this)));
+  }
+
+  render() {
+    return (
+      <section id="HOME__section" className="parallax" style="background-image:url('/img/t47/t47_avatar.jpg')">
+        <div className="cover HOME__shade" style={{backgroundColor: `rgba(0, 7, 11, ${this.state.isShade ? 0.75 : 0.25})`}}></div>
+        <div className="container">
+          <div className="HOME__content text-white">
+            <img src='/img/t47/t47_name_2_line.gif' alt="Siqi Tian" width="480" style={this.state.isName ? tween.name.end : tween.name.start} />
+            <p className="text-white" style="margin-top: -10px;">&nbsp;</p>
+            <p className="text-white" style="line-height: 25px; height: 50px;">
+              <span dangerouslySetInnerHTML={{__html: this.state.title}} ></span>
+              <b class={`HOME__cursor ${this.state.isBlink ? "blink" : ""}`} >{(this.state.isShade & this.state.isName) ? "|" : ""}</b>
+            </p>
+          </div>
+          <div className="HOME__scroll_down">
+            <i className="fa fa-3x fa-fw fa-arrow-circle-down"></i>
+          </div>
+
+        </div>
+      </section>
+    );
+  }
+}
 
 
 export default HomeSection;
