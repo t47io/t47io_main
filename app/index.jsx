89c6f1b93f8f2cdd9a6f128f39827ab670479b86
t47io <t47@alumni.stanford.edu>
2017-01-09 15:27:05 -0500

    manual compile loading screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AsyncComponent from './components/loading/jsx/async.jsx';
-import HelixLoading from './components/loading/jsx/helix.jsx';
-
-
-const loader = (callback) => {
-  require.ensure(['./components/index/index.jsx'], () => {
-    const Main = require('./components/index/index.jsx');
-
-    callback(Main, {
-      ...(require('../public/config.json')),
-      svg: require('../public/data/git_contrib.svg')
-    });
-  }, 'main');
-}
-
+import Main from './components/index/index.jsx';
 
 ReactDOM.render(
-  <AsyncComponent loader={loader} placeholder={<HelixLoading />} />,
+  <Main {...(require('../public/config.json'))}
+    svg={require('../public/data/git_contrib.svg')} />,
   document.getElementById("app")
 );
+
+
+// import AsyncComponent from './components/loading/jsx/async.jsx';
+// import HelixLoading from './components/loading/jsx/helix.jsx';
+
+
+// const loader = (callback) => {
+//   require.ensure(['./components/index/index.jsx'], () => {
+//     const Main = require('./components/index/index.jsx');
+
+//     callback(Main, {
+//       ...(require('../public/config.json')),
+//       svg: require('../public/data/git_contrib.svg')
+//     });
+//   }, 'main');
+// }
+
+
+// ReactDOM.render(
+//   <AsyncComponent loader={loader} placeholder={<HelixLoading />} />,
+//   document.getElementById("app")
+// );
---
+++ webpack.render.js
@@ -0,0 +1,71 @@
+import fs from 'fs';
+import sass from 'node-sass';
+
+
+const CSS = sass.renderSync({
+  file: `${__dirname}/app/components/loading/scss/async.scss`,
+  outputStyle: 'compressed'
+}).css.toString();
+
+const logoAltSVG = fs.readFileSync('./app/components/common/img/t47_logo_alt.svg');
+const HTML = `
+  <div class="LOAD__container UTIL__image-RNA">
+    <div class="LOAD__content">
+      <a href="mailto:contact@t47.io" target="_blank" rel="noopener noreferrer external" class="LOAD__logo green-white">
+        ${logoAltSVG}
+      </a>
+  
+      <div class="row">
+        <div class="LOAD__helix center-block" style="width:75%;">
+          <div></div><div></div><div></div>
+          <div></div><div></div><div></div>
+          <div></div><div></div><div></div>
+          <div></div><div></div><div></div>
+          <div></div><div></div><div></div>
+          <div></div><div></div><div></div>
+          <div></div><div></div><div></div>
+          <div></div><div></div><div></div>
+          <div></div><div></div>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+const helixLoading = {CSS, HTML};
+
+
+const title = "SIQI TIAN - Full-Stack Web Developer & RNA Biochemistry Automator | T47.IO";
+
+const meta = [
+  {charset: "utf-8"},
+  {
+    'http-equiv': "Content-Type",
+    content: "text/html; charset=UTF-8"
+  },
+  {
+    name: "description",
+    content: "Personal portfolio of Siqi Tian, a full-stack web designer and developer, as well as an RNA biochemist (PhD) from Stanford University."
+  },
+  {
+    name: "keywords",
+    content: "Siqi Tian, Portfolio, Personal Website, Design, RNA, Full-Stack Developer, t47io"
+  },
+  {
+    name: "author",
+    content: "Siqi Tian"
+  },
+  {
+    name: "viewport",
+    content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+  },
+  {
+    name: "robots",
+    content: "noodp, noydir"
+  }
+];
+
+
+const env = require('./server/config/env.json');
+const googleAnalytics = {trackingID: env.ga_tracker};
+
+export {title, meta, helixLoading, googleAnalytics};
