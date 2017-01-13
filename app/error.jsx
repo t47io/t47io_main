bc67800a8d6f9bde1bd6220ed38efc977699a909
t47io <t47@alumni.stanford.edu>
2017-01-13 17:00:29 -0500

    custom error rendering to multiple html files

--- app/error.jsx
+++ app/error.jsx
@@ -1,37 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'whatwg-fetch';
-
-// import Main from './index/index.jsx';
 
 
-// fetch('/config.json')
-// .then((response) => response.json())
-// .then((json) => {
-  ReactDOM.render(
-    <h1>ERROR</h1>,
-    document.getElementById("app")
-  );
-// });
+ReactDOM.render(  
+  <h1>placeholder</h1>,
+  document.getElementById("app")
+);
 
 
-// import AsyncComponent from './components/loading/jsx/async.jsx';
-// import HelixLoading from './components/loading/jsx/helix.jsx';
-
-
-// const loader = (callback) => {
-//   require.ensure(['./components/index/index.jsx'], () => {
-//     const Main = require('./components/index/index.jsx');
-
-//     callback(Main, {
-//       ...(require('../public/config.json')),
-//       svg: require('../public/data/git_contrib.svg')
-//     });
-//   }, 'main');
-// }
-
-
-// ReactDOM.render(
-//   <AsyncComponent loader={loader} placeholder={<HelixLoading />} />,
-//   document.getElementById("app")
-// );
---
+++ build/render_error.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {renderToString} from 'react-dom/server';
+import fs from 'fs';
+import path from 'path';
+import sass from 'node-sass';
+
+import Components from '../app/error/jsx/pages.jsx';
+import Footer from '../app/error/jsx/footer.jsx';
+
+
+const codes = {
+  400: 'BadRequest',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'NotFound',
+  405: 'MethodNotAllowed',
+  500: 'InternalServerError',
+  502: 'BadGateway',
+  503: 'ServiceUnavailable',
+  201: 'Created'
+};
+
+const loadFileSync = (filename) => fs.readFileSync(path.join(__dirname, filename), 'utf8');
+
+
+const baseHTML = loadFileSync('../public/error.html');
+const logoAltSVG = loadFileSync('../app/common/img/t47_logo_alt.svg');
+const copySVG = loadFileSync('../app/error/img/copyright.svg');
+const ccSVG = loadFileSync('../app/error/img/creative-commons.svg');
+const footerHTML = renderToString(<Footer logo={logoAltSVG} copy={copySVG} cc={ccSVG} />);
+const CSS = sass.renderSync({
+  file: path.join(__dirname, '../app/error/scss/error.scss'),
+  outputStyle: 'compressed'
+}).css.toString();
+
+
+[400, 401, 403, 404, 405, 500, 502, 503, 201].map((code) => {
+  const Component = Components[codes[code]];
+  const bodyHTML = `${renderToString(<Component />)}<hr/>${footerHTML}`;
+  const HTML = baseHTML.replace("<div class=\"body\" id=\"app\"></div>", bodyHTML).replace("html{}", CSS);
+
+  fs.writeFileSync(path.join(__dirname, `../public/${code}.html`), HTML, 'utf8');
+});
