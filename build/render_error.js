f4cd82459d1417e6b310533e7ba2bc77d1b812c7
t47io <t47@alumni.stanford.edu>
2017-01-14 16:29:10 -0700

    finish error SCSS; use purify

--- build/render_error.js
+++ build/render_error.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {renderToString} from 'react-dom/server';
 import fs from 'fs';
 import path from 'path';
+import purify from 'purify-css';
 import sass from 'node-sass';
 
 import Components from '../app/error/jsx/pages.jsx';
@@ -28,16 +29,16 @@ const logoAltSVG = loadFileSync('../app/common/img/t47_logo_alt.svg');
 const copySVG = loadFileSync('../app/error/img/copyright.svg');
 const ccSVG = loadFileSync('../app/error/img/creative-commons.svg');
 const footerHTML = renderToString(<Footer logo={logoAltSVG} copy={copySVG} cc={ccSVG} />);
-const CSS = sass.renderSync({
-  file: path.join(__dirname, '../app/error/error.scss'),
-  outputStyle: 'compressed'
+const rawCSS = sass.renderSync({
+  file: path.join(__dirname, '../app/error/error.scss')
 }).css.toString();
 
 
 [400, 401, 403, 404, 405, 500, 502, 503, 201].map((code) => {
   const Component = Components[codes[code]];
-  let bodyHTML = `${renderToString(<Component />).slice(0, -6)}<hr/>${footerHTML}</div>`;
-  let HTML = baseHTML.replace("<div class=\"body\" id=\"app\"></div>", bodyHTML).replace("html{}", CSS);
+  const bodyHTML = `${renderToString(<Component />).slice(0, -6)}<hr/>${footerHTML}</div>`;
+  const cleanCSS = purify(bodyHTML, rawCSS, {minify: true});
+  const finalHTML = baseHTML.replace("<div class=\"body\" id=\"app\"></div>", bodyHTML).replace("html{}", cleanCSS);
 
-  fs.writeFileSync(path.join(__dirname, `../public/${code}.html`), HTML, 'utf8');
+  fs.writeFileSync(path.join(__dirname, `../public/${code}.html`), finalHTML, 'utf8');
 });
