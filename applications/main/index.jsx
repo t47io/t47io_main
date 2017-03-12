dc82ddf20cdde04d5c7865e0b0cc198d5b4a0f6f
t47io <t47@alumni.stanford.edu>
2017-03-11 16:10:28 -0800

    use thunk, conditional preact/devtools and redux-logger

--- applications/main/index.jsx
+++ applications/main/index.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'whatwg-fetch';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
 import Main from './containers/index.jsx';
+import reducer from './reducers/index.js';
 
+const middleware = [thunk];
+if (process.env.NODE_ENV !== 'production') {
+  require('preact/devtools');
 
-fetch('/config.json')
-.then(response => response.json())
-.then((json) => {
-  ReactDOM.render(
-    <Main {...json} />,
-    document.getElementById('app')
-  );
-});
+  const createLogger = require('redux-logger');
+  const logger = createLogger();
+  middleware.push(logger);
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Main />
+  </Provider>,
+  document.getElementById('app')
+);
 
 // import AsyncComponent from './components/loading/jsx/async.jsx';
 // import HelixLoading from './components/loading/jsx/helix.jsx';
