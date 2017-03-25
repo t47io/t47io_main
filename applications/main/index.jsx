dad6ced41344bbc4a8dd75d6fa793d5093cce397
t47io <t47@alumni.stanford.edu>
2017-03-25 00:16:38 -0700

    move dom.render() out of require.ensure

--- applications/main/index.jsx
+++ applications/main/index.jsx
@@ -16,45 +16,23 @@ if (process.env.NODE_ENV !== 'production') {
   const logger = createLogger();
   middleware.push(logger);
 }
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+);
 
 require.ensure([], (require) => {
   const json = require('../../public/config.json');
-
-  const store = createStore(
-    reducer,
-    applyMiddleware(...middleware)
-  );
   store.dispatch({
     type: LOAD_JSON_DATA,
     payload: { ...json },
   });
-
-  ReactDOM.render(
-    <Provider store={store}>
-      <Main />
-    </Provider>,
-    document.getElementById('app')
-  );
 }, 'config');
 
 
-// import AsyncComponent from './components/loading/jsx/async.jsx';
-// import HelixLoading from './components/loading/jsx/helix.jsx';
-
-
-// const loader = (callback) => {
-//   require.ensure(['./components/index/index.jsx'], () => {
-//     const Main = require('./components/index/index.jsx');
-
-//     callback(Main, {
-//       ...(require('../public/config.json')),
-//       svg: require('../public/data/git_contrib.svg')
-//     });
-//   }, 'main');
-// }
-
-
-// ReactDOM.render(
-//   <AsyncComponent loader={loader} placeholder={<HelixLoading />} />,
-//   document.getElementById("app")
-// );
+ReactDOM.render(
+  <Provider store={store}>
+    <Main />
+  </Provider>,
+  document.getElementById('app')
+);
