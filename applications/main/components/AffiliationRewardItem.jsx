76c150912463a9b341ec195bbec000565938e8f3
t47io <t47@alumni.stanford.edu>
2017-03-25 17:49:29 -0700

    fix affiliation spacer

--- applications/main/components/AffiliationRewardItem.jsx
+++ applications/main/components/AffiliationRewardItem.jsx
@@ -4,15 +4,22 @@ import React from 'react';
 const AffiliationRewardItem = ({
   year,
   title,
-}) => (
-  <tr>
-    <td>
-      <i className="text-gray">{year}</i>
-      &nbsp;&nbsp;&nbsp;&nbsp;
-    </td>
-    <td dangerouslySetInnerHTML={{ __html: title }} />
-  </tr>
-);
+}) => {
+  if (!year) {
+    return (
+      <tr className="UTIL__spacer-sm" />
+    );
+  }
+
+  return (
+    <tr>
+      <td>
+        <i className="text-gray">{year}</i>
+      </td>
+      <td dangerouslySetInnerHTML={{ __html: title }} />
+    </tr>
+  );
+};
 
 AffiliationRewardItem.propTypes = {
   year: React.PropTypes.number,
