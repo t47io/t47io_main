538dad097bb3c959551e73349afd95243b35cf3d
t47io <t47@alumni.stanford.edu>
2017-04-07 22:05:33 -0700

    add email report to admin with full cron result

---
+++ server/cron/data_report.js
@@ -0,0 +1,52 @@
+import colors from 'colors';
+
+import {
+  DEBUG,
+  EMAIL_RECV,
+  SMTP,
+} from '../config.js';
+
+const cron = require('../../config/cron.json');
+
+
+const formatPubs = () => {
+  let sumCitation = 0;
+
+  Object.keys(cron.citations).forEach((tag) => {
+    sumCitation += parseInt(cron.citations[tag], 10) || 0;
+  });
+  return sumCitation;
+};
+
+const emailAdmin = content => (
+  SMTP.sendMail({
+    to: EMAIL_RECV,
+    subject: '[t47io] Google Scholar Citation Update',
+    text: content,
+  }, (err) => {
+    if (err) { console.log(err); }
+  })
+);
+
+
+try {
+  const content = `
+${new Date().toISOString()}
+
+Google Scholar Citations:
+${JSON.stringify(cron.citations, null, 2)}
+  ( total = ${formatPubs()} )
+
+GitHub Contributions:
+${JSON.stringify(cron.gitContrib, null, 2)}
+
+SSL Certificates:
+${JSON.stringify(cron.https, null, 2)}
+  `;
+  if (!DEBUG) { emailAdmin(content); }
+
+  console.log(`${colors.green('SUCCESS')}: Notified admin on cron data results.`);
+} catch (err) {
+  console.log(`${colors.red('ERROR')}: Failed to notify admin on cron data results.`);
+  console.log(err);
+}
