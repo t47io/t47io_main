700f49387ba246a31822ce50af7ecb85293eab50
t47io <t47@alumni.stanford.edu>
2017-04-23 14:07:11 -0700

    update config json examples

--- applications/project/constants/labelTypes.js
+++ applications/project/constants/labelTypes.js
@@ -1,15 +1,18 @@
 const LABELS = {
-  server: 'Production',
+  server: 'Server',
+  prod: 'Production',
   demo: 'Demonstration',
   theme: 'Theme',
 };
 const COLORS = {
   server: 'yellow',
+  prod: 'yellow',
   demo: 'gray',
   theme: 'purple',
 };
 const KEYS = [
   'server',
+  'prod',
   'demo',
   'theme',
 ];
