27b3cf61317d6e8a2cb6b7f93a71f50706b8ef41
t47io <t47@alumni.stanford.edu>
2017-04-23 00:16:36 -0700

    bump version 2.1.6

---
+++ applications/project/constants/projectTypes.js
@@ -0,0 +1,19 @@
+export const DASLAB = 'daslab';
+export const PRIMERIZE = 'primerize';
+export const RMDB = 'rmdb';
+export const ETERNA = 'eterna';
+export const HITRACE = 'hitrace';
+export const SPINDLE = 'spindle';
+export const RIBOKIT = 'ribokit';
+export const CELICA = 'celica';
+
+export const PROJECT_LIST = [
+  DASLAB,
+  PRIMERIZE,
+  RMDB,
+  ETERNA,
+  HITRACE,
+  SPINDLE,
+  RIBOKIT,
+  CELICA,
+];
