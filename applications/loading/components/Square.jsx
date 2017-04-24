6e2abcac6e591c20983ca51521890d866d32993e
t47io <t47@alumni.stanford.edu>
2017-04-23 19:23:33 -0700

    add square loader

---
+++ applications/loading/components/Square.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Square = () => (
+  <div className="LOAD__square center-block">
+    <div className="row clearfix">
+      <div className="LOAD__square-block LOAD__square-block--1" />
+      <div className="LOAD__square-block LOAD__square-block--2" />
+      <div className="LOAD__square-block LOAD__square-block--3" />
+    </div>
+    <div className="row clearfix">
+      <div className="LOAD__square-block LOAD__square-block--8" />
+      <div className="LOAD__square-block LOAD__square-block--9" />
+      <div className="LOAD__square-block LOAD__square-block--4" />
+    </div>
+    <div className="row clearfix">
+      <div className="LOAD__square-block LOAD__square-block--7" />
+      <div className="LOAD__square-block LOAD__square-block--6" />
+      <div className="LOAD__square-block LOAD__square-block--5" />
+    </div>
+  </div>
+);
+
+
+export default Square;
