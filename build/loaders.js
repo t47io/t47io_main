5424e92b7300cdecdabde3150cf1580cb7dac34a
t47io <t47@alumni.stanford.edu>
2017-06-11 16:56:59 -0700

    update loaders config

--- build/loaders.js
+++ build/loaders.js
@@ -1,81 +1,82 @@
 import autoprefixer from 'autoprefixer';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-
 const loaders = (SSR = false) => {
-  const loader = [{
-    test: /\.js(x)?$/i,
-    exclude: /node_module/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['es2015'],
-      },
-    },
-  }, {
-    test: /\.scss$/i,
-    use: ExtractTextPlugin.extract({
-      fallback: 'style-loader',
+  const loader = [
+    {
+      test: /\.js(x)?$/i,
+      exclude: /node_module/,
       use: [
         {
-          loader: 'css-loader',
-          options: { importLoaders: 1 },
-        },
-        {
-          loader: 'postcss-loader',
+          loader: 'babel-loader',
           options: {
-            sourceMap: false,
-            plugins: () => ([autoprefixer]),
+            presets: ['es2015'],
           },
         },
-        'resolve-url-loader',
-        {
-          loader: 'sass-loader',
-          options: { sourceMap: true },
-        },
+        // 'eslint-loader',
       ],
-    }),
-  }, {
-    test: /\.woff$/i,
-    use: {
-      loader: 'url-loader',
-      options: {
-        limit: 25600,
-        mimetype: 'application/x-font-woff',
-        name: 'font/[hash:6].[ext]',
+    },
+    {
+      test: /\.scss$/i,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader?importLoaders=1',
+          {
+            loader: 'postcss-loader',
+            options: { plugins: () => ([autoprefixer]) },
+          },
+          'resolve-url-loader',
+          'sass-loader?sourceMap',
+        ],
+      }),
+    },
+    {
+      test: /\.woff$/i,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 25600,
+          mimetype: 'application/x-font-woff',
+          name: 'font/[hash:6].[ext]',
+        },
       },
     },
-  }, {
-    test: /\.(woff2|ttf|eot)$/i,
-    use: {
-      loader: 'file-loader',
-      options: { name: 'font/[hash:6].[ext]' },
-    },
-  }, {
-    test: /\.(png|jpg|gif)$/i,
-    use: {
-      loader: 'url-loader',
-      options: {
-        limit: 25600,
-        name: 'image/[hash:6].[ext]',
+    {
+      test: /\.(woff2|ttf|eot)$/i,
+      use: {
+        loader: 'file-loader',
+        options: { name: 'font/[hash:6].[ext]' },
       },
     },
-  }, {
-    test: /\.svg$/i,
-    use: {
-      loader: 'svg-inline-loader',
-      options: {
-        removeSVGTagAttrs: false,
-        mimetype: 'image/svg+xml',
+    {
+      test: /\.(png|jpg|gif)$/i,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 25600,
+          name: 'image/[hash:6].[ext]',
+        },
       },
     },
-  }, {
-    test: /\.mp3$/i,
-    use: {
-      loader: 'file-loader',
-      options: { name: 'audio/[hash:6].[ext]' },
+    {
+      test: /\.svg$/i,
+      use: {
+        loader: 'svg-inline-loader',
+        options: {
+          removeSVGTagAttrs: false,
+          mimetype: 'image/svg+xml',
+        },
+      },
     },
-  }];
+    {
+      test: /\.mp3$/i,
+      use: {
+        loader: 'file-loader',
+        options: { name: 'audio/[hash:6].[ext]' },
+      },
+    },
+  ];
 
   if (SSR) {
     loader.unshift({
