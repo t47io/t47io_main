7a232c70b418d0e64cf8b3635c19039b5b787012
t47io <t47@alumni.stanford.edu>
2017-07-03 22:18:25 -0700

    finish vendor brand logo svg

--- applications/main/components/Images.js
+++ applications/main/components/Images.js
@@ -2,3 +2,17 @@ export const imgAvatar = require('../images/t47_avatar.jpg');
 export const imgName = require('../images/t47_name.png');
 
 export const imgPhone = require('../images/t47_phone.png');
+
+export const svgBrands = {
+  babel: require('../../vendor/js-logo/babel.svg'),
+  express: require('../../vendor/js-logo/express.svg'),
+  nginx: require('../../vendor/js-logo/nginx.svg'),
+  nodejs: require('../../vendor/js-logo/nodejs.svg'),
+  pm2: require('../../vendor/js-logo/pm2.svg'),
+  preact: require('../../vendor/js-logo/preact.svg'),
+  redux: require('../../vendor/js-logo/redux.svg'),
+  ubuntu: require('../../vendor/js-logo/ubuntu.svg'),
+  vultr: require('../../vendor/js-logo/vultr.svg'),
+  webpack: require('../../vendor/js-logo/webpack.svg'),
+  yarn: require('../../vendor/js-logo/yarn.svg'),
+};
