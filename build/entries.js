4499ee6d0db5a5317fedb89c1003fee55f3bd311
t47io <t47@alumni.stanford.edu>
2017-10-01 12:15:40 -0700

    remove bootstraprc appStyles

--- build/entries.js
+++ build/entries.js
@@ -5,31 +5,16 @@ const entries = (DEBUG = true) => {
   const chunkNames = CHUNK_NAMES(DEBUG);
   const entry = {
     [chunkNames.main]: [
+      'web-animations-js/web-animations.min.js',
       'bootstrap-loader',
       './applications/main/index.jsx',
     ],
     [chunkNames.project]: [
+      'web-animations-js/web-animations.min.js',
       'bootstrap-loader',
       './applications/project/index.jsx',
     ],
     [chunkNames.error]: './applications/error/index.jsx',
-    [chunkNames.vendor]: [
-      'bootstrap-loader',
-      'es6-promise/auto',
-      'react',
-      'react-document-meta',
-      'react-redux',
-      'react-tooltip',
-      'react-waypoint',
-      'react-web-animation',
-      'reduce-reducers',
-      'redux',
-      'redux-logger',
-      'redux-thunk',
-      'smoothscroll',
-      'web-animations-js',
-      'whatwg-fetch',
-    ],
   };
 
   if (DEBUG) {
