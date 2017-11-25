4caf69cbf1006e42abc958d2877f2e61f1a40b58
t47io <t47@alumni.stanford.edu>
2017-11-24 17:49:57 -0800

    logo svg  remove from jsx

--- applications/common/components/Images.js
+++ applications/common/components/Images.js
@@ -1,8 +1,15 @@
+import React from 'react';
+
+import SvgLogo from '../images/t47_logo.svg';
+import SvgLogoAlt from '../images/t47_logo_alt.svg';
+
 import SvgSEA from '../images/fg_city_sea.svg';
 import SvgSFO from '../images/fg_city_sfo.svg';
 
 
 export {
+  SvgLogo,
+  SvgLogoAlt,
   SvgSEA,
   SvgSFO,
 };
