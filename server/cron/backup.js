6615fa1ab9312dc6e351fb6c4190f99e8608e795
t47io <t47@alumni.stanford.edu>
2020-02-08 13:38:23 -0800

    cron:backup use async

--- server/cron/backup.js
+++ server/cron/backup.js
@@ -1,73 +1,85 @@
-import shell from 'shelljs';
-
-import { PATH } from '../env.js';
+import {
+  ROOT,
+  PATH,
+} from '../env.js';
 import { FILE_NAMES } from '../config.js';
+import { exec } from '../util.js';
 import logger from '../logger.js';
 
 const log = logger('cron:backup');
 
 
-const prepareFolder = () => {
-  shell.rm('-rf', PATH.BACKUP);
-  shell.mkdir('-p', `${PATH.BACKUP}/json`, `${PATH.BACKUP}/svg`, `${PATH.BACKUP}/nginx`);
-  shell.mkdir('-p', `${PATH.BACKUP}/json/main`, `${PATH.BACKUP}/json/project`, `${PATH.BACKUP}/json/repository`);
-};
+const backupJson = async () => {
+  await exec(`mkdir -p ${PATH.BACKUP}/json/main ${PATH.BACKUP}/json/project ${PATH.BACKUP}/json/repository`);
+  await Promise.all([
+    exec(`cp -R ${PATH.CONFIG}/*.json ${PATH.BACKUP}/json`),
+    exec(`cp -R ${PATH.CONFIG}/main/*.json ${PATH.BACKUP}/json/main`),
+    exec(`cp -R ${PATH.CONFIG}/project/*.json ${PATH.BACKUP}/json/project`),
+    exec(`cp -R ${PATH.CONFIG}/repository/*.json ${PATH.BACKUP}/json/repository`),
+  ]);
 
-const backupJSON = () => {
-  shell.cp('-R', 'config/*.svg', `${PATH.BACKUP}/svg`);
-
-  shell.cp('-R', 'config/*.json', `${PATH.BACKUP}/json`);
-  shell.cp('-R', 'config/main/*.json', `${PATH.BACKUP}/json/main`);
-  shell.cp('-R', 'config/project/*.json', `${PATH.BACKUP}/json/project`);
-  shell.cp('-R', 'config/repository/*.json', `${PATH.BACKUP}/json/repository`);
-  shell.rm('-rf', `${PATH.BACKUP}/**/*.example.json`);
-  shell.rm('-rf', `${PATH.BACKUP}/json/stats-*.json`);
-  shell.rm('-rf', `${PATH.BACKUP}/json/main.json`, `${PATH.BACKUP}/json/project.json`);
-
+  await Promise.all([
+    exec(`rm -rf ${PATH.BACKUP}/**/*.example.json`),
+    exec(`rm -rf ${PATH.BACKUP}/json/stats-*.json`),
+    exec(`rm -rf ${PATH.BACKUP}/json/main.json ${PATH.BACKUP}/json/project.json`),
+  ]);
   log.debug('JSON config backed up.');
 };
 
-const backupNginx = () => {
-  shell.cp('-R', '/etc/nginx/nginx.conf', `${PATH.BACKUP}/nginx`);
-  shell.cp('-R', '/etc/nginx/t47io/*.conf', `${PATH.BACKUP}/nginx`);
+const backupSvg = async () => {
+  await exec(`mkdir -p ${PATH.BACKUP}/svg`);
+  await exec(`cp -R ${PATH.CONFIG}/repository/account/*.svg ${PATH.BACKUP}/svg`);
+};
+
+const backupNginx = async () => {
+  await exec(`mkdir -p ${PATH.BACKUP}/nginx`);
+  await Promise.all([
+    exec(`cp -R /etc/nginx/nginx.conf ${PATH.BACKUP}/nginx`),
+    exec(`cp -R /etc/nginx/t47io/*.conf ${PATH.BACKUP}/nginx`),
+  ]);
 
   log.debug('NGINX config backed up.');
 };
 
-const backupMisc = () => {
-  shell.cp('/etc/init/uwsgi.conf', PATH.BACKUP);
-  shell.cp('/etc/hosts', PATH.BACKUP);
-  shell.cp('/etc/apt/sources.list', PATH.BACKUP);
-  shell.cp('/home/admin/.bash_profile', `${PATH.BACKUP}/bash_profile`);
-  shell.cp('/home/admin/.dircolors', `${PATH.BACKUP}/dircolors`);
-  shell.cp('/home/admin/.nanorc', `${PATH.BACKUP}/nanorc`);
+const backupMisc = async () => {
+  await Promise.all([
+    exec(`cp /etc/init/uwsgi.conf ${PATH.BACKUP}`),
+    exec(`cp /etc/hosts ${PATH.BACKUP}`),
+    exec(`cp /etc/apt/sources.list ${PATH.BACKUP}`),
+    exec(`cp /home/admin/.bash_profile ${PATH.BACKUP}/bash_profile`),
+    exec(`cp /home/admin/.dircolors ${PATH.BACKUP}/dircolors`),
+    exec(`cp /home/admin/.nanorc ${PATH.BACKUP}/nanorc`),
+  ]);
 
   log.debug('System config backed up.');
 };
 
-const createTgz = () => {
+const createTgz = async () => {
   const tarFilename = FILE_NAMES.BACKUP.replace('.gz', '');
 
-  shell.cd(PATH.BACKUP);
-  shell.cd('..');
-  shell.exec(`tar -vcf ${tarFilename} backup/`);
-  shell.exec(`zopfli ${tarFilename}`);
-  shell.rm('-rf', tarFilename);
+  await exec(`cd ${ROOT} && tar -vcf ${tarFilename} backup/`);
+  await exec(`zopfli ${ROOT}/${tarFilename}`);
+  await exec(`rm -rf ${ROOT}/${tarFilename}`);
 
   log.debug('Backup TGZ file created.');
 };
 
 
-try {
-  prepareFolder();
-  backupJSON();
-  backupNginx();
-  backupMisc();
-  createTgz();
+(async () => {
+  try {
+    await exec(`rm -rf ${PATH.BACKUP}`);
+    await Promise.all([
+      backupJson(),
+      backupSvg(),
+      backupNginx(),
+      backupMisc(),
+    ]);
+    await createTgz();
 
-  log.info('System settings and data backed up.');
-} catch (err) {
-  console.error(err);
-  log.error('Failed to backup System settings and data.');
-  process.exit(1);
-}
+    log.info('System settings and data backed up.');
+  } catch (err) {
+    console.error(err);
+    log.error('Failed to backup System settings and data.');
+    process.exit(1);
+  }
+})();
